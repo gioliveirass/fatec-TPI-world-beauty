9c590cf2d27ddb7fd794e7e03bf5cb392f82c298
Giovana O. Silva <78885451+gioliveirass@users.noreply.github.com>
2021-11-17 14:44:04 -0300

    Adicionando 10 serviços iniciais

--- src/utils/insercaoServicosIniciais.ts
+++ src/utils/insercaoServicosIniciais.ts
@@ -10,7 +10,37 @@ class InsercaoServicosIniciais extends Cadastro {
   }
 
   public cadastrar(): void {
-    this.servicos.push(new Servico("Remoção de rugas", 1, 500));
+    // 1
+    this.servicos.push(new Servico("Manicure", 1, 15));
+
+    // 2
+    this.servicos.push(new Servico("Pedicure", 2, 15));
+
+    // 3
+    this.servicos.push(new Servico("Design de sombrancelhas", 3, 20));
+
+    // 4
+    this.servicos.push(new Servico("Corte de cabelo", 4, 50));
+
+    // 5
+    this.servicos.push(new Servico("Pintura de cabelo", 5, 50));
+
+    // 6
+    this.servicos.push(new Servico("Remoção  de manchas na pele", 6, 300));
+
+    // 7
+    this.servicos.push(new Servico("Aplicação de botox", 7, 150));
+
+    // 8
+    this.servicos.push(new Servico("Remoção de rugas", 8, 500));
+
+    // 9
+    this.servicos.push(new Servico("Tratamento para emagrecimento ", 9, 1100));
+
+    // 10
+    this.servicos.push(
+      new Servico("Tratamento para redução de medidas", 10, 1000)
+    );
   }
 }
 
